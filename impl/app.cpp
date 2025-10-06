971e42896c1b521fb580ecf9586ab42b6efda3f7
Frederick Ziola <frederick.ziola@gmail.com>
2025-10-06 14:41:48 -0400

    Fix small typo

--- impl/app.cpp
+++ impl/app.cpp
@@ -1,13 +1,13 @@
 /*
-* frc-pathgen/include/app.cpp
+* frc-pathgen/impl/app.cpp
 * Copyright (c) 2025 Frederick Ziola et al. (New Lothrop Robotics)
 * Licensed under MIT. see LICENSE file in the repository root.
 *   Use, copy, modify, and distribute as needed, simply credit the original author.
 *   Because we are programmers, not lawyers!
 */
 
-#include "app.hpp"
 #include <spdlog/spdlog.h>
+#include "app.hpp"
 
 namespace frc_pathgen {
 
