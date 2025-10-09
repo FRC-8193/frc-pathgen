048ab0f5f62a4965757fbe6a394133fc778ca946
Frederick Ziola <frederick.ziola@gmail.com>
2025-10-09 16:42:36 -0400

    Hopefully improve support for MSYS2

--- imgui.cmake
+++ imgui.cmake
@@ -7,4 +7,4 @@ add_library(ImGui STATIC
   ${CMAKE_CURRENT_SOURCE_DIR}/extern/imgui/backends/imgui_impl_sdlrenderer2.cpp
 )
 
-target_include_directories(ImGui PUBLIC ${CMAKE_CURRENT_SOURCE_DIR}/extern/imgui/)
+target_include_directories(ImGui PUBLIC ${CMAKE_CURRENT_SOURCE_DIR}/extern/imgui/ ${CMAKE_CURRENT_SOURCE_DIR}/extern/imgui/backends)
--- impl/app.cpp
+++ impl/app.cpp
@@ -8,8 +8,8 @@
 
 #include <spdlog/spdlog.h>
 #include <imgui.h>
-#include <backends/imgui_impl_sdl2.h>
-#include <backends/imgui_impl_sdlrenderer2.h>
+#include <imgui_impl_sdl2.h>
+#include <imgui_impl_sdlrenderer2.h>
 #include "app.hpp"
 #include "SDL_timer.h"
 #include "world.hpp"
