2b20368725d4e64f1b6d6590eec8592234e140f2
Frederick Ziola <frederick.ziola@gmail.com>
2025-10-09 09:23:07 -0400

    Add a generic PID controller

---
+++ include/pid.hpp
@@ -0,0 +1,29 @@
+/*
+* frc-pathgen/include/pid.hpp
+* Copyright (c) 2025 Frederick Ziola et al. (New Lothrop Robotics)
+* Licensed under MIT. see LICENSE file in the repository root.
+*   Use, copy, modify, and distribute as needed, simply credit the original author.
+*   Because we are programmers, not lawyers!
+*/
+
+#pragma once
+
+template<typename T, typename K=T>
+struct PIDController {
+  K kP, kI, kD;
+
+  T update(const T &target, const T &current, float dt) {
+    T error = target - current;
+    this->accum_error += error * dt;
+    T diff_error = (error - this->last_error) / dt;
+    this->last_error = error;
+
+    return this->kP * error + this->kI * this->accum_error + this->kD * diff_error;
+  }
+
+  void reset() {
+    this->last_error = this->accum_error = {};
+  }
+private:
+  T last_error = {}, accum_error = {};
+};
